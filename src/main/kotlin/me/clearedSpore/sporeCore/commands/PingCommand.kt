c1f2d4b65ad55f47e71f5570b1e7da649615ff19
Clearedspore <spore1325@gmail.com>
2025-11-25 15:57:47 +0100

    feat: Added /ping & /whois

---
+++ src/main/kotlin/me/clearedSpore/sporeCore/commands/PingCommand.kt
@@ -0,0 +1,47 @@
+package me.clearedSpore.sporeCore.commands
+
+import co.aikar.commands.BaseCommand
+import co.aikar.commands.annotation.*
+import co.aikar.commands.contexts.OnlinePlayer
+import me.clearedSpore.sporeAPI.util.CC.blue
+import me.clearedSpore.sporeAPI.util.CC.green
+import me.clearedSpore.sporeAPI.util.CC.orange
+import me.clearedSpore.sporeAPI.util.CC.red
+import me.clearedSpore.sporeAPI.util.CC.translate
+import me.clearedSpore.sporeAPI.util.CC.yellow
+import me.clearedSpore.sporeAPI.util.Message.sendErrorMessage
+import me.clearedSpore.sporeCore.util.Perm
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+@CommandAlias("ping")
+@CommandPermission(Perm.PING)
+class PingCommand : BaseCommand() {
+
+    @Default
+    @CommandCompletion("@players")
+    @Syntax("[player]")
+    fun onPing(sender: Player, @Optional onlineTarget: OnlinePlayer?) {
+        val target = onlineTarget?.player ?: sender
+        val ping = target.ping
+
+        val formattedPing = when {
+            ping < 90 -> "$ping".green()
+            ping < 120 -> "&e$ping"
+            ping < 150 -> "$ping".orange()
+            else -> "$ping".red()
+        }
+
+        if (target == sender) {
+            sender.sendMessage("Your ping is $formattedPing".blue())
+        } else {
+
+            if(!sender.hasPermission(Perm.PING_OTHERS)){
+                sender.sendErrorMessage("You don't have permission to check other players their ping!".red())
+                return
+            }
+
+            sender.sendMessage("${target.name}'s ping is $formattedPing".blue())
+        }
+    }
+}
---
+++ src/main/kotlin/me/clearedSpore/sporeCore/commands/WhoisCommand.kt
@@ -0,0 +1,85 @@
+package me.clearedSpore.sporeCore.commands
+
+import co.aikar.commands.BaseCommand
+import co.aikar.commands.annotation.*
+import me.clearedSpore.sporeAPI.util.CC.blue
+import me.clearedSpore.sporeAPI.util.CC.white
+import me.clearedSpore.sporeAPI.util.TimeUtil
+import me.clearedSpore.sporeCore.SporeCore
+import me.clearedSpore.sporeCore.extension.PlayerExtension.userFail
+import me.clearedSpore.sporeCore.features.currency.CurrencySystemService
+import me.clearedSpore.sporeCore.features.stats.StatService
+import me.clearedSpore.sporeCore.user.UserManager
+import me.clearedSpore.sporeCore.util.Perm
+import org.bukkit.Bukkit
+import org.bukkit.command.CommandSender
+
+@CommandAlias("whois")
+@CommandPermission(Perm.WHOIS)
+class WhoisCommand : BaseCommand() {
+
+    @Default()
+    @CommandCompletion("@players")
+    @Syntax("<player>")
+    fun onWhois(sender: CommandSender, targetName: String) {
+        val target = Bukkit.getOfflinePlayer(targetName)
+        val user = UserManager.get(target)
+
+        if (!target.hasPlayedBefore() || user == null) {
+            sender.userFail()
+            return
+        }
+
+        val playtime = TimeUtil.formatDuration(StatService.getTotalPlaytime(user))
+        val weeklyPlaytime = TimeUtil.formatDuration(StatService.getWeeklyPlaytime(user))
+        val lastJoin = StatService.getLastJoin(user)
+        val firstJoin = StatService.getFirstJoin(user)
+        val firstServerIP = user.firstServerIP
+        val lastServerIP = user.lastServerIP
+
+        val currencyName = CurrencySystemService.currencyName
+        val credits = CurrencySystemService.getBalance(user)
+        val formatted = CurrencySystemService.format(credits)
+
+        val currencyEnabled = SporeCore.instance.coreConfig.features.currency.enabled
+        val punishmentsEnabled = SporeCore.instance.coreConfig.features.punishments
+
+        sender.sendMessage("-----------------------------")
+        sender.sendMessage("Who is $targetName?".blue())
+        sender.sendMessage("")
+
+        if (currencyEnabled) {
+            sender.sendMessage("$currencyName: ".white() + "$formatted")
+            sender.sendMessage("")
+        }
+
+        sender.sendMessage("Playtime: ".white() + "$playtime".blue())
+        sender.sendMessage("Weekly Playtime: ".white() + "$weeklyPlaytime".blue())
+        sender.sendMessage("Last Join: ".white() + lastJoin.blue())
+        sender.sendMessage("First Join: ".white() + firstJoin.blue())
+        sender.sendMessage("")
+        sender.sendMessage("First server IP: ".white() + "$firstServerIP".blue())
+        sender.sendMessage("Last server IP: ".white() + "$lastServerIP".blue())
+        sender.sendMessage("")
+
+        if (punishmentsEnabled) {
+            val activePunishments = user.getActivePunishments()
+            sender.sendMessage("Punishments: ".white())
+            if (activePunishments.isEmpty()) {
+                sender.sendMessage("None".blue())
+            } else {
+                activePunishments.forEach { pun ->
+                    val type = pun.type.displayName
+                    val reason = pun.reason
+                    val staff = pun.getPunisher()!!.playerName
+                    val expires = pun.getDurationFormatted()
+                    sender.sendMessage("- $type by $staff: $reason ($expires)".blue())
+                }
+            }
+            sender.sendMessage("")
+        }
+
+        sender.sendMessage("-----------------------------")
+
+    }
+}
