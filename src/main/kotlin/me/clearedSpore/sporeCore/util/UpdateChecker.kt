62a85a897b681c2604fbb143e459e47120c75649
Clearedspore <151540868+Clearedspore@users.noreply.github.com>
2026-08-07 13:54:25 +0200

    feat: Fixed Update checker

--- src/main/kotlin/me/clearedSpore/sporeCore/util/UpdateChecker.kt
+++ src/main/kotlin/me/clearedSpore/sporeCore/util/UpdateChecker.kt
@@ -1,11 +1,14 @@
 package me.clearedSpore.sporeCore.util
 
+import com.google.gson.JsonParser
+import me.clearedSpore.sporeAPI.annotation.RegisterListener
 import me.clearedSpore.sporeAPI.task.Tasks
 import me.clearedSpore.sporeAPI.util.CC.blue
 import me.clearedSpore.sporeAPI.util.Logger
 
 import me.clearedSpore.sporeCore.SporeCore
 import org.bukkit.Bukkit
+import org.json.JSONArray
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -15,34 +18,38 @@ import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
 
-class UpdateChecker() : Listener {
+@RegisterListener
+class UpdateChecker : Listener {
 
-    val plugin = SporeCore.instance
+    val plugin get() = SporeCore.instance
 
-    val resourceId: Int = 121185
+    val projectSlug: String = "sporecore"
     var latestVersion: String? = null
     var updateAvailable: Boolean = false
 
     init {
-        Bukkit.getPluginManager().registerEvents(this, plugin)
         checkForUpdates()
     }
 
     fun checkForUpdates() {
         Tasks.runAsync {
             try {
-                val url = URL("https://api.spigotmc.org/legacy/update.php?resource=$resourceId")
+                val url = URL("https://api.modrinth.com/v2/project/$projectSlug/version?include_changelog=false")
                 val connection = url.openConnection() as HttpURLConnection
                 connection.requestMethod = "GET"
+                connection.setRequestProperty("User-Agent", "SporeCore/${plugin.description.version} (UpdateChecker)")
                 connection.connectTimeout = 5000
                 connection.readTimeout = 5000
 
-                BufferedReader(InputStreamReader(connection.inputStream)).use { reader ->
-                    latestVersion = reader.readLine()
+                val response = BufferedReader(InputStreamReader(connection.inputStream)).use { it.readText() }
+                val versions = JsonParser.parseString(response).asJsonArray
+
+                if (versions.size() > 0) {
+                    latestVersion = versions[0].asJsonObject.get("version_number").asString
                 }
 
                 val currentVersion = plugin.description.version
-                if (currentVersion != latestVersion) {
+                if (latestVersion != null && currentVersion != latestVersion) {
                     updateAvailable = true
 
                     Bukkit.getConsoleSender().sendMessage("")
